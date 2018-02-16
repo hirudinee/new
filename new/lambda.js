bc6eccebe5efa7dbc89527916c4ef232c52fbacb
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-02-16 10:58:41 +0530

    test

--- new/lambda.js
+++ new/lambda.js
@@ -1,4 +1,5 @@
 let AWS = require('aws-sdk');
+const s3 = new AWS.S3();
 const ddb = new AWS.DynamoDB.DocumentClient();
 exports.handler = function (event, context, callback) {
 	ddb.get({
@@ -11,8 +12,22 @@ exports.handler = function (event, context, callback) {
 			//your logic goes here
 		}
 	});
+	s3.getBucketLocation({
+		'Bucket': "hiru.test"
+	}).promise()
+		.then(data => {
+			console.log(data);           // successful response
+			/*
+			data = {
+				LocationConstraint: "us-west-2"
+			}
+			*/
+		})
+		.catch(err => {
+			console.log(err, err.stack); // an error occurred
+		});
 
-	
+
 
 	//test
 	callback(null, 'Successfully executed');
